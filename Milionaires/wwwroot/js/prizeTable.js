a9127073bad0fc43e0dc93394805d96b4018569b
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-01-04 21:42:19 +0100

    remove borders - prepair the PrizeTable for new style

--- Milionaires/wwwroot/js/prizeTable.js
+++ Milionaires/wwwroot/js/prizeTable.js
@@ -11,46 +11,44 @@
             table.classList.add("normalColor");
             switch (i) {
                 case 10:
-                    table.innerText = 1000000;
+                    table.innerText = `1000000 zł`;
                     table.id = "RowQuestion10";
-                    table.style.borderTop = "2px solid black";
                     break;
                 case 9:
-                    table.innerText = 500000;
+                    table.innerText = "500000 zł";
                     table.id = "RowQuestion9";
                     break;
                 case 8:
-                    table.innerText = 250000;
+                    table.innerText = "250000 zł";
                     table.id = "RowQuestion8";
                     break;
                 case 7:
-                    table.innerText = 150000;
+                    table.innerText = "150000 zł";
                     table.id = "RowQuestion7";
                     break;
                 case 6:
-                    table.innerText = 75000;
+                    table.innerText = "75000 zł";
                     table.id = "RowQuestion6";
                     break;
                 case 5:
-                    table.innerText = 40000;
+                    table.innerText = "40000 zł";
                     table.id = "RowQuestion5";
                     break;
                 case 4:
-                    table.innerText = 10000;
+                    table.innerText = "10000 zł";
                     table.id = "RowQuestion4";
                     break;
                 case 3:
-                    table.innerText = 5000;
+                    table.innerText = "5000 zł";
                     table.id = "RowQuestion3";
                     break;
                 case 2:
-                    table.innerText = 2000;
+                    table.innerText = "2000 zł";
                     table.id = "RowQuestion2";
                     break;
                 case 1:
-                    table.innerText = 500;
+                    table.innerText = "500 zł";
                     table.id = "RowQuestion1";
-                    table.style.borderBottom = "none";
                     break;
             }
             this.prizeTable.appendChild(table);
